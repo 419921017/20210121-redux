4f816fe0aaae8cd50a6eecb900d742d3cc5a16a1
power_840 <liuyu19921017@gmail.com>
2021-01-27 21:57:16 +0800

    feat: thunk.withExtraArgument

--- src/react-thunk/index.js
+++ src/react-thunk/index.js
@@ -4,15 +4,15 @@
  * @Author: power_840
  * @Date: 2021-01-27 20:46:41
  * @LastEditors: power_840
- * @LastEditTime: 2021-01-27 21:46:21
+ * @LastEditTime: 2021-01-27 21:53:09
  */
-function createThunkMiddleware() {
+function createThunkMiddleware(exrtaArgument) {
   return ({ getState, dispatch }) => (next) => (action) => {
     if (typeof action === "function") {
       // 函数执行完成后不会继续往下执行, 直接return出来
       // 这个里面dispatch是经过包装后的dispatch, 不是原生的store.dispatch
       // return出来后会重新执行dispatch, 这里的dispatch是传入执行函数中的包装后的dispatch
-      return action(getState, dispatch);
+      return action(getState, dispatch, exrtaArgument);
     } else {
       return next(action);
     }
@@ -20,5 +20,7 @@ function createThunkMiddleware() {
 }
 
 const thunk = createThunkMiddleware();
+
+thunk.withExtraArgument = createThunkMiddleware;
 
 export default thunk;
