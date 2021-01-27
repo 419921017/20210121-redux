7211ad7675af40a1f326d110f65be28b62db51d9
power_840 <liuyu19921017@gmail.com>
2021-01-27 21:46:58 +0800

    feat: thunk 注释

--- src/react-thunk/index.js
+++ src/react-thunk/index.js
@@ -4,11 +4,14 @@
  * @Author: power_840
  * @Date: 2021-01-27 20:46:41
  * @LastEditors: power_840
- * @LastEditTime: 2021-01-27 21:32:30
+ * @LastEditTime: 2021-01-27 21:46:21
  */
 function createThunkMiddleware() {
   return ({ getState, dispatch }) => (next) => (action) => {
     if (typeof action === "function") {
+      // 函数执行完成后不会继续往下执行, 直接return出来
+      // 这个里面dispatch是经过包装后的dispatch, 不是原生的store.dispatch
+      // return出来后会重新执行dispatch, 这里的dispatch是传入执行函数中的包装后的dispatch
       return action(getState, dispatch);
     } else {
       return next(action);
